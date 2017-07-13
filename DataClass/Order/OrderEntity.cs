56231d90e46d08ee45db8aba89a9bb507d35d233
HansJao <vigorhan@gmail.com>
2017-07-14 00:29:33 +0800

    new feature
    新增使用者名稱

--- DataClass/Order/OrderEntity.cs
+++ DataClass/Order/OrderEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Hungyi.DataClass.User;
 
 namespace Hungyi.DataClass.Order
 {
@@ -9,6 +10,7 @@ namespace Hungyi.DataClass.Order
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public int UserID { get; set; }
+        public string userName { get; set; }
         public int TotalPrice { get; set; }
         public int TotalCost { get; set; }
         public int TotalQuantity { get; set; }
